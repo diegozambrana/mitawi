b00596489fe8dbbddb72936fe718ca8bcebed085
Diego Armando Zambrana Arze <contacto@diegozambrana.com>
2024-09-02 17:36:08 -0400

    fix style issues in mobile version

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,6 +1,8 @@
+"use client";
 import {
   Avatar,
   Box,
+  Burger,
   Group,
   Menu,
   MenuDropdown,
@@ -11,10 +13,24 @@ import {
 } from "@mantine/core";
 import { IconLogout, IconSettings } from "@tabler/icons-react";
 import Link from "next/link";
+import { FC } from "react";
 
-export const Header = () => {
+type HeaderProps = {
+  BurgerOpened: boolean;
+  onClickBurger: () => void;
+};
+
+export const Header: FC<HeaderProps> = ({ BurgerOpened, onClickBurger }) => {
   return (
     <Group justify="space-between" mx={16} mt={8}>
+      <Box className="burger__button">
+        <Burger
+          opened={BurgerOpened}
+          onClick={onClickBurger}
+          hiddenFrom="sm"
+          size="sm"
+        />
+      </Box>
       <Link href="/" style={{ textDecoration: "none" }}>
         <Title order={1}>Mitawi</Title>
       </Link>
